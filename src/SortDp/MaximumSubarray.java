4c6823d2f19800c54cfb551721074bd95d595158
DESKTOP-OI19NCO\ppyum <doeon93@naver.com>
2023-03-12 20:25:34 +0900

    add : DP (53. Maximum Subarray)

---
+++ src/SortDp/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package SortDp;
+
+// https://leetcode.com/problems/maximum-subarray/
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int len = nums.length, max = dp[0];
+        for(int i=1; i<len; i++) {
+            dp[i] = nums[i] + (dp[i-1]>0 ? dp[i-1] : 0);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
